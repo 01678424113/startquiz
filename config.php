83fe833166783d726558d1c3ea06de79b7ef861e
Nguyen Lam <lamnn@mediaz.vn>
2018-07-15 10:47:33 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+ return array (
+  'DB_CONNECTION' => 'mysql',
+  'DB_HOST' => '127.0.0.1',
+  'DB_DATABASE' => 'socioquiz',
+  'DB_USERNAME' => 'root',
+  'DB_PASSWORD' => '',
+  'ADMIN_USERNAME' => 'admin',
+  'ADMIN_PASSWORD' => '123456',
+);
